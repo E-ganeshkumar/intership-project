c71f24030138cebfa5071666851475a8e3fb7978
Ganesh <ganeshediga36@gmail.com>
2026-09-03 21:05:32 +0530

    update

--- home/jobs/views.py
+++ home/jobs/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from rest_framework.filters import SearchFilter
 from .models import Job
 from .serializers import JobSerializer
@@ -7,16 +7,15 @@ from .serializers import JobSerializer
 class JobListCreateAPIView(generics.ListCreateAPIView):
     queryset = Job.objects.select_related('employer')
     serializer_class = JobSerializer
+    permission_classes = [permissions.IsAuthenticated]
     filter_backends = [SearchFilter]
-    search_fields = [
-        'title',
-        'location',
-        'skills'
-    ]
+    search_fields = ['title', 'location', 'skills']
+
+    def perform_create(self, serializer):
+        serializer.save(employer=self.request.user)
+
 
 class JobDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
-
-
-        
+    permission_classes = [permissions.IsAuthenticated]
